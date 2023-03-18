186873a6645d41b21f2de93c0e98dd2d50b5c802
ashrithjacob <ashrithjacob@gmail.com>
2023-03-18 16:12:50 +0100

    create data object

---
+++ numpy/ANN.py
@@ -0,0 +1,11 @@
+class NN():
+    def __init__(self, input_layer, hidden_layers, output_layer):
+        self.input_layer = input_layer
+        self.hidden_layers = hidden_layers
+        self.output_layers = output_layers
+
+    def activation_fn(self):
+    def model(self):
+    #TODO
+    #weights
+    #hyperparams
