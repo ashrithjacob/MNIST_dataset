66e9b9fbac09d1a198c5395b24eeb67707eb6b4b
ashrithjacob <ashrithjacob@gmail.com>
2023-03-23 02:05:46 +0100

    temporary fixes

--- numpy_model/ANN.py
+++ numpy_model/ANN.py
@@ -6,13 +6,17 @@ class NN():
         self.layers = []
         self.W = []
         self.B = []
+        self.a=[]
+        self.z=[]
         self.dW=[]
         self.dB=[]
         self.da=[]
         self.dz=[]
+        self.buf_dW=[]
+        self.buf_dB=[]
         self.lr = lr
         self.mom = mom
-        self.classes =10
+        self.classes = 10
         self.layers.append(input_layer)
         for layer in hidden_layers:
             self.layers.append(layer)
@@ -31,22 +35,26 @@ class NN():
         self.dB.append(None)
         self.da.append(None)
         self.dz.append(None)
+        self.a.append(None)
+        self.z.append(None)
+        self.buf_dW.append(None)
+        self.buf_dB.append(None)
 
-    def step(self, input_vals):
+    def step(self, input_vals, predicted_vals):
         m= input_vals.shape[0]
         self.forward(input_vals)# creating forward
         self.backward(m,predicted_vals)# finding d_params
         self.sgd()#updating params
 
     def forward(self, input_vals):
-        self.a = [input_vals]
+        self.a = [input_vals] # extra dim for a
         for i in range(self.num_layers-1):
-            z=np.matmul(self.a[i],np.transpose(self.W[i])) + self.B[i]
-            self.a.append(self.activation(z))
+            self.z[i]=np.matmul(self.a[i],np.transpose(self.W[i])) + self.B[i]
+            self.a.append(self.activation(self.z[i]))
 
     def activation(self,z):
         # check if final layer
-        if z.shape[1] == self.layers[self.num_layers]:
+        if z.shape[1] == self.layers[self.num_layers-1]:
             #logsoftmax
             omega = np.sum(np.exp(z), axis=1, keepdims=True)
             z = np.log(np.exp(z)/omega)
@@ -56,25 +64,43 @@ class NN():
         return z
 
     def backward(self,m, predicted_vals):
-        Y= np.zeroes(m,self.classes)
+        Y= np.zeros((m,self.classes))
         Y[np.arange(predicted_vals.size), predicted_vals] = 1.0
         self.da.append((-1.0/m)*Y)# extra dimension for da
         for i in range(self.num_layers-1,-1,-1):
-            self.dz[i]=[d_activation(z[i])* self.da[i+1]]
+            self.dz[i]=[self.d_activation(self.z[i])* self.da[i+1]]
             self.dW[i]=(1.0/m)*np.matmul(np.transpose(self.a),self.dz[i])
             self.dB[i]=(1.0/m)*np.sum(self.dz[i],axis =0, keepdims=True)
             self.da[i]=np.matmul(self.dz[i],self.W[i])
 
-    def sgd():
-        
+    def d_activation(self,z):
+        # check if final layer
+        if z.shape[1] == self.layers[self.num_layers-1]:
+            #derivitive of logsoftmax
+            omega = np.sum(np.exp(z), axis=1, keepdims=True)
+            z = 1-(np.exp(z)/omega)
+        else:
+            #derivitive of Relu
+            z[z<0] = 0.0
+            z[z>0] = 1.0
+        return z
 
 
+    def sgd(self):
+        for i in range(self.num_layers-1):
+            if self.buf_dW[i] is None:
+                self.buf_dW[i] = self.dW[i]
+            else:
+                self.buf_dW[i] *= self.mom + self.dW[i]
+            if self.buf_dB[i] is None:
+                self.buf_dB[i] = self.dB[i]
+            else:
+                self.buf_dB[i] *= self.mom + self.dB[i] 
+            
+            self.W[i] -= self.lr*self.buf_dW[i]
+            self.B[i] -= self.lr*self.buf_dB[i]
 
-
-    #def model(self):
-    #TODO
-    # Weights initialisation
-    # Forward prop
-    # Backward prop
-    # Updating weights via SGD
-    # Hyperparam setting/tuning
+#TODO
+#clean code
+#test
+# write main
--- tests/test_ANN.py
+++ tests/test_ANN.py
@@ -3,19 +3,23 @@ import numpy as np
 from numpy_model.ANN import NN
 
 model = NN(728, [128, 64], 10)
-m =  64
-x = np.random.rand(m,728)
-model.forward(x)
+m = 64
+x = np.random.rand(m, 728)
+y = np.floor(np.random.rand(m) * 10).astype(int)
+model.step(x,y)
+
 
 def test_weight_init():
     for i in range(model.num_layers):
-        assert model.W[i].shape == (model.layers[i+1], model.layers[i])
-        assert np.max(model.W[i]) <= 1.0/(model.layers[i])**0.5
-        assert np.min(model.W[i]) >= -1.0/(model.layers[i])**0.5
+        assert model.W[i].shape == (model.layers[i + 1], model.layers[i])
+        assert np.max(model.W[i]) <= 1.0 / (model.layers[i]) ** 0.5
+        assert np.min(model.W[i]) >= -1.0 / (model.layers[i]) ** 0.5
+
 
 def test_forward_dims():
     for i in range(model.num_layers):
-        assert model.a[i].shape == (m,model.layers[i])
+        assert model.a[i].shape == (m, model.layers[i])
+
 
 def test_backward_dims():
     for i in range(model.num_layers):
@@ -23,6 +27,3 @@ def test_backward_dims():
         assert model.dz[i].shape == model.z[i].shape
         assert model.dW[i].shape == model.W[i].shape
         assert model.dB[i].shape == model.B[i].shape
-
-
-
